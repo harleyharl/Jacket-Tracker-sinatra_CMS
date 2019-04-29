fdc3f9b38e94d0c427b6a479fca1508e5dc0a6c4
harleyharl <harleymteden@gmail.com>
2019-04-29 07:57:21 -0700

    refactored and removed unused code

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,6 +1,2 @@
 class BrandsController < Sinatra::Base
-  
-  include Slugifiable #so we can use as instance method
-  extend Slugifiable #so we can use as class method for methods like Artist.find_by_slug
-
 end
