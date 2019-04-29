8d18245ab1761f3efdd743469ede9907b747b20e
harleyharl <harleymteden@gmail.com>
2019-04-29 07:59:28 -0700

    removed unused modules and used helper methods to DRY up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,17 @@
-require_relative '../models/concerns/slugifiable.rb'
-
 class UsersController < ApplicationController
 
-  include Slugifiable #so we can use as instance method
-  extend Slugifiable #so we can use as class method for methods like Artist.find_by_slug
-
   get '/signup' do
-
-    if logged_in? #logged in user can't even get to signup page
-      @user = User.find_by_id(session[:user_id])
+    if logged_in? #logged in user is redirected to their jackets index
+      @user = current_user
       user_slug = @user.slug
       redirect "/#{user_slug}/jackets"
     else
-      # clear_errors
       erb :"users/new"
     end
   end
 
-
   post '/signup' do
-    # binding.pry
-    if user_exists
+    if user_exists #see ApplicationController methods
       session[:fail_user_exists] = "That user already exists"
       redirect '/signup'
     elsif params[:username].empty? || params[:password].empty?
@@ -39,7 +30,7 @@ class UsersController < ApplicationController
 
   get '/login' do
     if logged_in?
-      @user = User.find_by_id(session[:user_id])
+      @user = current_user
       user_slug = @user.slug
       redirect "/#{user_slug}/jackets"
     else
@@ -49,20 +40,19 @@ class UsersController < ApplicationController
   end
 
   post "/login" do
-      user = User.find_by(username: params[:username])
-      user_slug = user.slug
-      if user && user.authenticate(params[:password])
-          session[:user_id] = user.id
-          redirect "/#{user_slug}/jackets"
-      else
-          erb :'users/login'
-      end
+    @user = current_user
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      user_slug = @user.slug
+      redirect "/#{user_slug}/jackets"
+    else
+      erb :'users/login'
+    end
   end
 
   post '/logout' do
     logout!
     redirect '/'
   end
-
 
 end
