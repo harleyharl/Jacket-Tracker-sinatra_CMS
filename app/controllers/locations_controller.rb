6365d05f42958e71425ba3d7743d2258354128fb
harleyharl <harleymteden@gmail.com>
2019-04-29 07:58:08 -0700

    refactored

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,24 +1,10 @@
 class LocationsController < ApplicationController
 
-  get '/locations' do
-    @locations = Location.all
-    erb :'/locations/index'
-  end
-
-
   get '/:user_slug/locations/:location_id' do
-
-    # binding.pry
     @user = User.find_by_id(session[:user_id])
-    # user_slug = @user.slug
-
     @location = Location.find_by(id: params[:location_id])
-    # location_slug = @location.slug
-
     @jackets = @location.jackets
-
     erb :'/locations/show'
   end
-
 
 end
