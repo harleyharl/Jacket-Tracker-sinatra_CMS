4687e6882ad91df36c8fdf2ec0b2280d06ec2aac
harleyharl <harleymteden@gmail.com>
2019-04-30 17:39:27 -0700

    removed notes

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,6 @@ ENV['SINATRA_ENV'] ||= "development"
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  # :database => "db/nyc#{ENV['SINATRA_ENV']}.sqlite"
   :database => "db/development.sqlite"
 )
 
