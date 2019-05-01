50df8cd23fde507fb46fae7de3600f351340e967
harleyharl <harleymteden@gmail.com>
2019-05-01 10:07:34 -0700

    built method protecting one user from accessing another user's locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,20 @@
 class LocationsController < ApplicationController
 
   get '/:user_slug/locations/:location_id' do
-    @user = User.find_by_id(session[:user_id])
-    @location = Location.find_by(id: params[:location_id])
-    @jackets = @location.jackets
-    erb :'/locations/show'
+    if current_user_logged_in?
+      @user = current_user
+      if @user.locations.include?(Location.find_by(id: params[:location_id]))
+        @location = Location.find_by(id: params[:location_id])
+      else
+        session[:wrong_url] = "Oops, you can't see somebody else's locations!"
+        redirect '/'
+      end
+      @jackets = @location.jackets
+      erb :'/locations/show'
+    else
+      session[:wrong_url] = "Oops, you can't see somebody else's jackets!"
+      redirect "/login"
+    end
   end
 
 end
