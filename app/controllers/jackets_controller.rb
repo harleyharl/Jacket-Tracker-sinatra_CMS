93f6762f92c75bacd512583ff27f42414a722c25
harleyharl <harleymteden@gmail.com>
2019-05-03 09:00:00 -0700

    refactored routes with redirect_if_wrong_user, and updated helper methods with their new names

--- app/controllers/jackets_controller.rb
+++ app/controllers/jackets_controller.rb
@@ -1,35 +1,27 @@
 class JacketsController < ApplicationController
 
   get '/:user_slug/jackets' do
-    if current_user_logged_in?
-      @user = current_user
-      @jackets = @user.jackets
-      erb :'/jackets/index'
-    else
-      session[:wrong_url] = "Oops, you can't see somebody else's jackets!"
-      redirect "/login"
-    end
+    redirect_if_wrong_user
+    @user = current_user
+    @jackets = @user.jackets
+    erb :'/jackets/index'
   end
 
   get '/:user_slug/jackets/new' do
-    if !current_user_logged_in?
-      session[:wrong_url] = "Oops, you can't see somebody else's jackets!"
-      redirect "/login"
-    else
-      @user = current_user
-      @locations = @user.locations
-      @jacket_types = @user.jackets.all.collect do |jacket|
-        jacket.jacket_type
-      end
-      erb :'/jackets/new'
+    redirect_if_wrong_user
+    @user = current_user
+    @locations = @user.locations
+    @jacket_types = @user.jackets.all.collect do |jacket|
+      jacket.jacket_type
     end
+    erb :'/jackets/new'
   end
 
   post '/jackets' do
     if !logged_in?
       redirect "/login"
     else
-      @user = User.find_by(id: session[:user_id])
+      @user = current_user
       @jacket = Jacket.new
       #takes info for jacket type(text box overrides any radio buttons ticked)
       if params[:jacket][:new_jacket_type] && !params[:jacket][:new_jacket_type].empty? #if theres text input
@@ -62,60 +54,49 @@ class JacketsController < ApplicationController
   end
 
   get '/:user_slug/jackets/:jacket_slug' do
-    if current_user_logged_in?
-      @user = current_user
-      if @user.jackets.include?(Jacket.find_by_slug(params[:jacket_slug]))
-        @jacket = Jacket.find_by_slug(params[:jacket_slug])
-      else
-        session[:wrong_url] = "Oops, you can't see somebody else's jackets!"
-        redirect '/'
-      end
-      erb :'jackets/show'
+    redirect_if_wrong_user
+    @user = current_user
+    if @user.jackets.include?(Jacket.find_by_slug(params[:jacket_slug]))
+      @jacket = Jacket.find_by_slug(params[:jacket_slug])
     else
       session[:wrong_url] = "Oops, you can't see somebody else's jackets!"
-      redirect "/login"
+      redirect '/'
     end
+    erb :'jackets/show'
   end
 
   get '/:user_slug/jackets/:jacket_slug/edit' do
-    if !current_user_logged_in?
-      session[:wrong_url] = "Oops, you can't move somebody else's jackets!"
-      redirect "/login"
+    redirect_if_wrong_user
+    @user = current_user
+    if @user.jackets.include?(Jacket.find_by_slug(params[:jacket_slug]))
+      @jacket = Jacket.find_by_slug(params[:jacket_slug])
     else
-      @user = current_user
-      if @user.jackets.include?(Jacket.find_by_slug(params[:jacket_slug]))
-        @jacket = Jacket.find_by_slug(params[:jacket_slug])
-      else
-        session[:wrong_url] = "Oops, you can't move somebody else's jackets!"
-        redirect '/'
-      end
-      @locations = @user.locations
-      erb :'jackets/edit'
+      session[:wrong_url] = "Oops, you can't move somebody else's jackets!"
+      redirect '/'
     end
+    @locations = @user.locations
+    erb :'jackets/edit'
   end
 
   patch '/:user_slug/jackets/:jacket_slug' do  #updates a jacket
-    if !logged_in?
-      redirect "/login"
-    else
-      @user = current_user
-      @jacket = @user.jackets.find_by_slug(params[:jacket_slug])
-      # takes text input or on radio button input
-      if params[:jacket][:new_location] && !params[:jacket][:new_location].empty?
-        @location = Location.find_or_create_by(name: params[:jacket][:new_location].chomp)
-        @jacket.location = @location #overwrite jackets location
-      elsif params[:jacket][:location_id] && !!params[:jacket][:location_id] #if a location box is ticked -
-        @location = Location.find_by(id: params[:jacket][:location_id])
-        @jacket.location = @location #find the location by id and set the association
-      end
-      @location.jackets << @jacket
-      erb :'jackets/show'
+    redirect_if_wrong_user
+    @user = current_user
+    @jacket = @user.jackets.find_by_slug(params[:jacket_slug])
+    # takes text input or on radio button input
+    if params[:jacket][:new_location] && !params[:jacket][:new_location].empty?
+      @location = Location.find_or_create_by(name: params[:jacket][:new_location].chomp)
+      @jacket.location = @location #overwrite jackets location
+    elsif params[:jacket][:location_id] && !!params[:jacket][:location_id] #if a location box is ticked -
+      @location = Location.find_by(id: params[:jacket][:location_id])
+      @jacket.location = @location #find the location by id and set the association
     end
+    @location.jackets << @jacket
+    erb :'jackets/show'
   end
 
   delete '/jackets/:id/delete' do
     @jacket = Jacket.find_by_id(params[:id])
-    @user = User.find_by(id: session[:user_id])
+    @user = current_user
     if logged_in? && @user.jackets.include?(@jacket)
       @jacket.delete
       redirect to "#{@user.slug}/jackets"
